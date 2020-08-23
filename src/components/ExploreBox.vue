5a79a983193e334196623b44560b991188153dac
Hasan Çamözü <h.camozuu@gmail.com>
2020-08-23 20:19:45 +0300

    explore.completed

---
+++ src/components/ExploreBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <figure>
+      <a href>
+        <img :src="src" alt />
+      </a>
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ExploreBox",
+  props: {
+    src: {
+      type: String,
+      default:
+        "https://instagram.fesb2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.129.1080.1080a/s640x640/118254876_400826077988652_2880450372014237813_n.jpg?_nc_ht=instagram.fesb2-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=FR1wwdXGL7wAX-0k5mQ&oh=e852138b9baaca373cd0504381e3fbeb&oe=5F6BF8CA"
+    }
+  }
+};
+</script>
---
+++ src/components/ExploreContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="boxs">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ExploreContainer"
+};
+</script>
+
+<style lang="scss" scoped>
+.boxs {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 28px;
+  margin-bottom: 28px;
+}
+@media screen and (max-width: 680px) {
+  .boxs {
+    grid-gap: 3px;
+    margin-bottom: 3px;
+  }
+}
+</style>
